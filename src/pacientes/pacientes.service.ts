e43003fdfa0229736e70626f64b834c0f6343ec3
gimontania <gimontania@gmail.com>
2026-09-18 14:35:30 -0300

    feat: CRUD completo de pacientes y medicos con decoradores de NestJS

--- src/pacientes/pacientes.service.ts
+++ src/pacientes/pacientes.service.ts
@@ -7,8 +7,50 @@ import { PrismaService } from "../prisma/prisma.service";
 export class PacientesService {
     constructor(private readonly prisma: PrismaService) {}
 
-    //obtiene todos los pacientes de la db
+    //get, pacientes -> devuelve todos los pacientes
     findAll() {
         return this.prisma.paciente.findMany();
+    }
+
+    //get, pacientes/:id -> busca un paciente por su id
+    findOne(id: number) {
+        return this.prisma.paciente.findUnique({
+            where: { id},
+        })
+    }
+
+    //post, pacientes -> crea un nuevo paciente
+    create(data: {
+        firstName: string
+        lastName: string
+        email: string
+        fechaNacimiento: Date
+    }) {
+        return this.prisma.paciente.create({
+            data,
+        })
+    }
+
+    //put, pacientes/:id -> actualiza un paciente existente
+    update(
+        id: number,
+        data: {
+            firstName?: string
+            lastName?: string
+            email?: string
+            fechaNacimiento?: Date
+        },
+    ) {
+        return this.prisma.paciente.update({
+            where: { id },
+            data,
+        })
+    }
+
+    //delete, pacientes/:id -> elimina un paciente
+    remove(id: number) {
+        return this.prisma.paciente.delete({
+            where: { id },
+        })
     }
 }
